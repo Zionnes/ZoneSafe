33996d16a5a0f964cb080328459e3e6d576dca0a
Zionnes <98216299+Zionnes@users.noreply.github.com>
2022-02-18 17:36:04 +0100

    Add files via upload

---
+++ JD_SafeZone/config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+Config.TopLeftInfoBox = true
+Config.ShowBorder = false
+Config.BorderHight = 5
+Config.MaxVehicleSpeed = false
+
+Config.pNotify = false
+Config.pNotifyEnterMessage = "Tu es en ZoneSafe."
+Config.pNotifyEnterType = "info"
+Config.pNotifyExitMessage = "Tu n'es plus en ZoneSafe."
+Config.pNotifyExitType = "info"
+
+Config.Zones = {
+    {
+        {245.05, -823.30, 30.00}, -- Parking Central
+        {198.97, -806.87, 31.07},
+        {228.86, -723.39, 39.55},
+        {276.42, -740.21, 35.58},
+    }
+}
+
+
+-- Version checker for when i update :)
+Config.versionCheck = "1.0.6"
